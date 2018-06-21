fce2893a016f25a9a2db8c2a890e2ab10dc80ca9
Vind <vindhya.polepalli@gmail.com>
2018-06-21 12:32:18 -0400

    testing-4 multibranch pipeline projects and changed Hello.java file

--- src/examples/Hello.java
+++ src/examples/Hello.java
@@ -49,7 +49,7 @@ public final class Hello extends HttpServlet {
         writer.println("</table>");
 
         writer.println("This is the output of a servlet that is part of");
-        writer.println("the Hello, World application.");
+        writer.println("the Hello, World web application.");
 
         writer.println("</body>");
         writer.println("</html>");
